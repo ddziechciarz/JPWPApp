a7384fdc0a920d4f4010846e8fc47c9ce07dc8e1
Michał Tajak <tajakmichal@gmail.com>
2023-04-19 19:46:44 +0200

    clear

--- charts.py
+++ charts.py
@@ -7,50 +7,53 @@ import math
 import datetime
 from bs4 import BeautifulSoup
 from login_pass import *
+from charts_read import *
+
 
 def login():
     driver = webdriver.Chrome()
     driver.get("https://home.solarman.cn/main.html")
-    time.sleep(10)
+    time.sleep(5)
     username_field = driver.find_element(By.NAME, "username")
     password_field = driver.find_element(By.NAME, "password")
 
-    # wprowadzenie danych użytkownika
     username_field.send_keys(LOGIN_SOLARMAN)
     password_field.send_keys(PASSWORD_SOLARMAN)
 
-    # zatwierdzenie formularza
     password_field.send_keys(Keys.RETURN)
-    time.sleep(20)
+    time.sleep(5)
     return driver
 
-def energy():
-    driver = login()
-    today = datetime.datetime.now()
-    month = today.strftime("%m")
-    day = today.strftime("%d")
-    driver.get('https://home.solarman.cn/cpro/epc/plantDetail/showCharts.json?plantId=1623212&type=1&date=2023%2F' + month + '%2F' + day + '&plantTimezoneId=39')
+
+def energy(driver, month, day):
+    driver.get(
+        'https://home.solarman.cn/cpro/epc/plantDetail/showCharts.json?plantId=1623212&type=1&date=2023%2F04%2F11&plantTimezoneId=39')
+    # driver.get('https://home.solarman.cn/cpro/epc/plantDetail/showCharts.json?plantId=1623212&type=1&date=2023%2F'+month+'%2F'+day+'&plantTimezoneId=39')
     charts_data = driver.page_source
     charts_split = charts_data.split('{')
-    charts_line = charts_split[len(charts_split)-1].split(',')
+    charts_line = charts_split[len(charts_split) - 1].split(',')
     charts_value = charts_line[2]
     energy = charts_value[9:len(charts_value)]
     return energy
 
+
 def energy_prediction():
-    driver = login()
+    month_percent = [0.039, 0.051, 0.081, 0.109, 0.114, 0.116, 0.123, 0.119, 0.096, 0.074, 0.045, 0.033]
+    yearly = 1275.6 * 6 * 0.7683
     today = datetime.datetime.now()
-    month = today.strftime("%m")
-    day = today.strftime("%d")
-    day_int = int(day) - 1
-    day = '0' + str(day_int)
-    driver.get('https://home.solarman.cn/cpro/epc/plantDetail/showCharts.json?plantId=1623212&type=1&date=2023%2F' + month + '%2F' + day + '&plantTimezoneId=39')
-    charts_data = driver.page_source
-    charts_split = charts_data.split('{')
-    charts_line = charts_split[len(charts_split)-1].split(',')
-    charts_value = charts_line[2]
-    energy = charts_value[9:len(charts_value)]
-    return energy
+    month = int(today.strftime("%m"))
+    day = int(today.strftime("%d"))
+    monthly = yearly * month_percent[month - 1]
+    daily = monthly / 30
+    average = daily / 0.57
+    f = open('data/energy_prediction.csv', mode='w')
+    for x in range(2, 9):
+        url = "https://www.accuweather.com/pl/pl/wojnarowa/274421/daily-weather-forecast/274421?day=" + str(x)
+        cloudiness = int(wheather_next(url, str(x))) / 100
+        day += 1
+        value = average * month_percent[month - 1] + average * (1 - cloudiness)
+        f.write(str(day) + ";" + str(value) + '\n')
+    f.close()
 
 
 def wheather_today(url):
@@ -73,20 +76,22 @@ def wheather_today(url):
         wind = wind_soup[1].text
 
     cloudiness_soup = soup.find("div", class_="right").find_all("span")
-    cloudiness = cloudiness_soup[2].text
+    cloudiness = cloudiness_soup[len(cloudiness_soup) - 1].text
 
-    sun_soup = soup.find("div", class_="sunrise-sunset").find("div", class_="left").find_all("span", class_="text-value")
+    sun_soup = soup.find("div", class_="sunrise-sunset").find("div", class_="left").find_all("span",
+                                                                                             class_="text-value")
     sunrise = sun_soup[0].text
     sunset = sun_soup[1].text
     fulldate = datetime.datetime.now()
     date = fulldate.strftime("%x")
     time = fulldate.strftime("%X")
-    dane = ['nazwa;wartosc', "Temperatura;"+temperature, "Predkosc wiatru;"+wind, "Zachmurzenie;"+cloudiness, "wschod;"+sunrise, "zachod;"+sunset, "data;"+date, "godzina;"+time]
+    data = ['nazwa;wartosc', "Temperatura;" + temperature, "Predkosc wiatru;" + wind, "Zachmurzenie;" + cloudiness,
+            "wschod;" + sunrise, "zachod;" + sunset, "data;" + date, "godzina;" + time]
     f = open('weather_tod.csv', mode='w')
-    for y in dane:
+    for y in data:
         text = y
-        f.write(text+'\n')
-    return cloudiness[0:len(cloudiness)-1]
+        f.write(text + '\n')
+    return cloudiness[0:len(cloudiness) - 1]
     f.close()
 
 
@@ -110,54 +115,34 @@ def wheather_next(url, day):
         wind = wind_soup[2].text
 
     cloudiness_soup = soup.find("div", class_="right").find_all("span")
-    cloudiness = cloudiness_soup[len(cloudiness_soup)-1].text
+    cloudiness = cloudiness_soup[len(cloudiness_soup) - 1].text
 
-    sun_soup = soup.find("div", class_="sunrise-sunset").find("div", class_="left").find_all("span", class_="text-value")
+    sun_soup = soup.find("div", class_="sunrise-sunset").find("div", class_="left").find_all("span",
+                                                                                             class_="text-value")
     sunrise = sun_soup[0].text
     sunset = sun_soup[1].text
 
-    dane = ['nazwa;wartosc', "Temperatura;"+temperature, "Predkosc wiatru;"+wind, "Zachmurzenie;"+cloudiness, "wschod;"+sunrise, "zachod;"+sunset]
-    f = open('weather_day'+day+'.csv', mode='w')
-    for y in dane:
+    data = ['nazwa;wartosc', "Temperatura;" + temperature, "Predkosc wiatru;" + wind, "Zachmurzenie;" + cloudiness,
+            "wschod;" + sunrise, "zachod;" + sunset]
+    f = open('weather_day' + day + '.csv', mode='w')
+    for y in data:
         text = y
-        f.write(text+'\n')
+        f.write(text + '\n')
     return cloudiness[0:len(cloudiness) - 1]
     f.close()
 
-def calculator_energy():
-    month_percent = [0.039, 0.051, 0.081, 0.109, 0.114, 0.116, 0.123, 0.119, 0.096, 0.074, 0.045, 0.033]
-    yearly = 1275.6 * 6 * 0.7683
-    today = datetime.datetime.now()
-    month = int(today.strftime("%m"))
-    monthly = yearly * month_percent[month - 1]
-    daily = monthly / 30
-    average = daily / 0.57
-    predictions = []
-    for x in range(1, 9):
-        if x != 1:
-            url = "https://www.accuweather.com/pl/pl/wojnarowa/274421/daily-weather-forecast/274421?day=" + str(x)
-            cloudiness = int(wheather_next(url, str(x))) / 100
-        else:
-            url = "https://www.accuweather.com/pl/pl/wojnarowa/274421/daily-weather-forecast/274421?day=1"
-            cloudiness = int(wheather_today(url)) / 100
-        value = average * (1 - cloudiness)
-        predictions.append(value)
-    return predictions
 
-def charts_today():
-    driver = login()
-    today = datetime.datetime.now()
-    month = today.strftime("%m")
-    day = today.strftime("%d")
-    driver.get('https://home.solarman.cn/cpro/epc/plantDetail/showCharts.json?plantId=1623212&type=1&date=2023%2F04%2F11&plantTimezoneId=39')
-    #driver.get('https://home.solarman.cn/cpro/epc/plantDetail/showCharts.json?plantId=1623212&type=1&date=2023%2F'+month+'%2F'+day+'&plantTimezoneId=39')
+def charts_today(driver, month, day):
+    driver.get(
+        'https://home.solarman.cn/cpro/epc/plantDetail/showCharts.json?plantId=1623212&type=1&date=2023%2F04%2F11&plantTimezoneId=39')
+    # driver.get('https://home.solarman.cn/cpro/epc/plantDetail/showCharts.json?plantId=1623212&type=1&date=2023%2F'+month+'%2F'+day+'&plantTimezoneId=39')
     charts_data = driver.page_source
     charts_split = charts_data.split('{')
     final_charts = []
-    for x in range(3, len(charts_split)-1):
+    for x in range(3, len(charts_split) - 1):
         charts_line = charts_split[x].split(',')
         charts_value = charts_line[3]
-        if x == len(charts_split)-2:
+        if x == len(charts_split) - 2:
             final_charts.append(charts_value[8:len(charts_value) - 2])
             break
         final_charts.append(charts_value[8:len(charts_value) - 1])
@@ -165,11 +150,12 @@ def charts_today():
     response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
     soup = BeautifulSoup(response.text, "html.parser")
-    sun_soup = soup.find("div", class_="sunrise-sunset").find("div", class_="left").find_all("span", class_="text-value")
+    sun_soup = soup.find("div", class_="sunrise-sunset").find("div", class_="left").find_all("span",
+                                                                                             class_="text-value")
     sunrise = sun_soup[0].text.split(':')
     hour = int(sunrise[0])
     minutes_float = float(sunrise[1])
-    minutes = math.ceil(minutes_float/10)*10
+    minutes = math.ceil(minutes_float / 10) * 10
     charts = []
     charts_time = []
     f = open('charts_today.csv', mode='w')
@@ -184,88 +170,53 @@ def charts_today():
         else:
             str_time = str_hour + ':' + str_minutes + str_minutes
         charts_time.append(str_time)
-        f.write(str_time+";"+z + '\n')
+        f.write(str_time + ";" + z + '\n')
         minutes += 10
     f.close()
     charts.append(charts_time)
     charts.append(final_charts)
-    driver.close()
-    return charts
 
-def charts_month_today():
-    driver = login()
-    time.sleep(20)
-    today = datetime.datetime.now()
-    month = today.strftime("%m")
-    driver.get('https://home.solarman.cn/cpro/epc/plantDetail/showCharts.json?plantId=1623212&type=2&date=2023%2F'+month+'&plantTimezoneId=39')
+
+def charts_month(driver, value_month, month):
+    if value_month == "last":
+        month_int = int(month) - 1
+        month = '0' + str(month_int)
+    driver.get(
+        'https://home.solarman.cn/cpro/epc/plantDetail/showCharts.json?plantId=1623212&type=2&date=2023%2F' + month + '&plantTimezoneId=39')
     charts_data = driver.page_source
     charts_split = charts_data.split('{')
     final_charts = []
-    for x in range(3, len(charts_split)-2):
+    for x in range(3, len(charts_split) - 2):
         charts_line = charts_split[x].split(',')
         charts_value = charts_line[3]
         if charts_value == '':
             final_charts.append('0.00')
         else:
             final_charts.append(charts_value[9:len(charts_value)])
-    charts = []
     charts_days = []
     day = 1
-    f = open('charts_month_today.csv', mode='w')
+    f = open('charts_month_' + value_month + '.csv', mode='w')
     for z in final_charts:
         if day < 10:
             day_str = '0' + str(day)
         else:
             day_str = str(day)
         charts_days.append(day_str)
-        f.write(day_str+";"+z + '\n')
+        f.write(day_str + ";" + z + '\n')
         day += 1
     f.close()
-    charts.append(charts_days)
-    charts.append(final_charts)
-    driver.close()
-    return charts
 
 
-def charts_month_last():
+def download():
     driver = login()
     today = datetime.datetime.now()
     month = today.strftime("%m")
-    month_int = int(month) - 1
-    month = '0' + str(month_int)
-    driver.get('https://home.solarman.cn/cpro/epc/plantDetail/showCharts.json?plantId=1623212&type=2&date=2023%2F'+month+'&plantTimezoneId=39')
-    charts_data = driver.page_source
-    charts_split = charts_data.split('{')
-    final_charts = []
-    for x in range(3, len(charts_split)-2):
-        charts_line = charts_split[x].split(',')
-        charts_value = charts_line[3]
-        if charts_value == '':
-            final_charts.append('0.00')
-        else:
-            final_charts.append(charts_value[9:len(charts_value)])
-    charts = []
-    charts_days = []
-    day = 1
-    f = open('charts_month_last.csv', mode='w')
-    for z in final_charts:
-        if day < 10:
-            day_str = '0' + str(day)
-        else:
-            day_str = str(day)
-        charts_days.append(day_str)
-        f.write(day_str+";"+z + '\n')
-        day += 1
-    f.close()
-    charts.append(charts_days)
-    charts.append(final_charts)
+    day = today.strftime("%d")
+    energy(driver, month, day)
+    charts_today(driver, month, day)
+    charts_month(driver, 'today', month)
+    charts_month(driver, 'last', month)
     driver.close()
-    return charts
 
 
-print(charts_today())
-
-#data_charts = charts_today()
-#for ch in range(0, len(data_charts[0])):
-#    print(data_charts[0][ch])
-#    print(data_charts[1][ch])
+download()
--- charts_read.py
+++ charts_read.py
@@ -1,6 +1,6 @@
 import csv
-import matplotlib
 import matplotlib.pyplot as plt
+
 
 def chart_today_img():
     time = []
@@ -13,53 +13,39 @@ def chart_today_img():
             value.append(float(split[1]))
     plt.figure(figsize=(10, 5))
     plt.plot(time, value, color="dodgerblue")
-    plt.xlabel('Godzina')
-    plt.ylabel('Wartość')
-    plt.title('Dzienna produkcja')
+    plt.xlabel('Hour')
+    plt.ylabel('Value')
+    plt.title('Daily production')
     plt.xticks(['6:00', '9:00', '12:00', '15:00', '18:00', '21:00'])
     plt.yticks([1, 2, 3, 4, 5, 6, 7], ['1kW', '2kW', '3kW', '4kW', '5kW', '6kW', '7kW'])
     plt.grid(axis='y')
     plt.savefig('img/charts_today.jpg', format='jpg')
     plt.close()
 
-def charts_month_last_img():
+
+def charts_month_img(month):
     time = []
     value = []
-    with open('data/charts_month_last.csv') as csvfile:
+    with open('data/charts_month_' + month + '.csv') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             split = row[0].split(';')
             time.append(split[0])
             value.append(float(split[1]))
     plt.figure(figsize=(10, 5))
-    plt.bar(time, value, color="dodgerblue",  edgecolor="white", width=1, linewidth=5)
-    plt.xlabel('Dzień')
-    plt.ylabel('Wartość')
-    plt.title('Miesieczna produkcja')
+    plt.bar(time, value, color="dodgerblue", edgecolor="white", width=1, linewidth=5)
+    plt.xlabel('Day')
+    plt.ylabel('Value')
+    plt.title('Monthly production')
     plt.yticks([10, 20, 30, 40], ['10kWh', '20kWh', '30kWh', '40kWh'])
     plt.grid(axis='y')
-    plt.savefig('img/charts_month_last.jpg', format='jpg')
+    plt.savefig('img/charts_month_' + month + '.jpg', format='jpg')
     plt.close()
 
 
-def charts_month_today_img():
-    time = []
-    value = []
-    with open('data/charts_month_today.csv') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            split = row[0].split(';')
-            time.append(split[0])
-            value.append(float(split[1]))
-    plt.figure(figsize=(10, 5))
-    plt.bar(time, value, color="dodgerblue",  edgecolor="white", width=1, linewidth=5)
-    plt.xlabel('Dzień')
-    plt.ylabel('Wartość')
-    plt.title('Miesieczna produkcja')
-    plt.yticks([10, 20, 30, 40], ['10kWh', '20kWh', '30kWh', '40kWh'])
-    plt.grid(axis='y')
-    plt.savefig('img/charts_month_today.jpg', format='jpg')
-    plt.close()
-chart_today_img()
-charts_month_last_img()
-charts_month_today_img()
+def all_img():
+    chart_today_img()
+    charts_month_img('today')
+    charts_month_img('last')
+
+all_img()
